5416a8540ecb5e816f600338e3ef34d62699100a
Eric Gu <eric5310@gmail.com>
2019-12-31 16:10:34 +0800

    formService

--- activitiChapter5/src/test/java/coreapi/FormServiceTest.java
+++ activitiChapter5/src/test/java/coreapi/FormServiceTest.java
@@ -6,9 +6,11 @@ import org.activiti.engine.FormService;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.form.FormProperty;
 import org.activiti.engine.form.StartFormData;
+import org.activiti.engine.form.TaskFormData;
 import org.activiti.engine.identity.Group;
 import org.activiti.engine.identity.User;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -16,6 +18,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,9 +42,21 @@ public class FormServiceTest {
         }
 
         Map<String, String> properties = Maps.newHashMap();
-        properties.put("message","value1");
+        properties.put("message", "value1");
         formService.submitStartFormData(processDefinition.getId(), properties);
 
+        Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
+        TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+        List<FormProperty> formProperties1 = taskFormData.getFormProperties();
+        for (FormProperty formProperty : formProperties1) {
+            log.info("formProperty = {}", ToStringBuilder.reflectionToString(formProperty));
+        }
+        Map<String, String> map = Maps.newHashMap();
+        map.put("yesOrNo", "yes");
+        formService.submitTaskFormData(task.getId(), map);
+
+        Task task1 = activitiRule.getTaskService().createTaskQuery().taskId(task.getId()).singleResult();
+        log.info("task1 = {}", task1);
 
     }
 }
