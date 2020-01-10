e8be94b188dde9baa5d21ed3b0f3740f4a4a8d10
Eric Gu <eric5310@gmail.com>
2020-01-10 14:07:09 +0800

    Runtime part1

---
+++ activitiChapter6/src/test/java/com.eric.activiti.dbentity/DBRuntimeTest.java
@@ -0,0 +1,44 @@
+package com.eric.activiti.dbentity;
+
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Map;
+
+@Slf4j
+public class DBRuntimeTest {
+
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule("activiti-mysql.cfg.xml");
+
+    @Test
+    public void testRuntime() {
+        activitiRule.getRepositoryService().createDeployment().name("二次审批流程")
+                .addClasspathResource("second_approve.bpmn20.xml")
+                .deploy();
+        Map<String, Object> variables = Maps.newHashMap();
+        variables.put("key1", "value1");
+        ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("second_approve", variables);
+
+    }
+
+    /**
+     * ACT_RU_IDENTITYLINK
+     */
+    @Test
+    public void testSetOwner() {
+        TaskService taskService = activitiRule.getTaskService();
+        Task task = taskService.createTaskQuery().processDefinitionKey("second_approve").singleResult();
+        taskService.setOwner(task.getId(), "user1");
+
+    }
+
+
+}
